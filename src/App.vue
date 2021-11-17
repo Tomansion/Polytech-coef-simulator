4f7f255e60c64c5219bea22e597acb4e7308ef9b
tom.mansion <tomansion@yahoo.fr>
2021-11-17 14:22:58 +0100

    added vue bootstrap added coef in yaml

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <b-card title="Card Title" style="max-width: 40rem">
+        <b-card-text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </b-card-text>
+
+        <b-button href="#" variant="primary">Go somewhere</b-button>
+      </b-card>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UE from "@/assets/coef/app5-info.yaml"; // gets modules coef from yaml file
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      ue: null,
+    };
+  },
+  created() {
+    // Read the UE yml in the assets
+    console.log(UE);
+  },
+};
 </script>
 
 <style>
